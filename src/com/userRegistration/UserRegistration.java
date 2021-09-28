15cb13b50f547e54bebbf1cb6ad1be3cd549dc99
poojakondoju <kondojupoojitha612@gmail.com>
2021-09-28 14:34:32 +0530

    Added:Last name starts with capital and has minimum 3 characters

--- src/com/userRegistration/UserRegistration.java
+++ src/com/userRegistration/UserRegistration.java
@@ -9,7 +9,7 @@ public class UserRegistration {
 	// Scanner class to accepting values at execution time.
 	Scanner scanner = new Scanner(System.in);
 	// Global variables.
-	public static final String firstName = "^[A-Z]{1}[a-zA-Z]{2,}+$"; // using Regex
+	public static final String userName = "^[A-Z]{1}[a-zA-Z]{2,}+$"; // using Regex
 
 	/*
 	 * Validating First Name.
@@ -17,11 +17,24 @@ public class UserRegistration {
 	public void firstName() {
 		System.out.println("Enter First Name");
 		String name = scanner.next();
-		// Comparing the name with firstName
-		if (name.matches(firstName))
+		// Comparing the name with userName
+		if (name.matches(userName))
 			System.out.println("Valid First Name");
 		else
 			System.out.println("In Valid First Name");
+	}
+
+	/*
+	 * Validating Last Name.
+	 */
+	public void LastName() {
+		System.out.println("Enter Last Name");
+		String name = scanner.next();
+		// Comparing the name with userName
+		if (name.matches(userName))
+			System.out.println("Valid Last Name");
+		else
+			System.out.println("In Valid Last Name");
 	}
 
 	/*
@@ -31,7 +44,8 @@ public class UserRegistration {
 		System.out.println("Welcome to User Registration");
 		// creating the object reference variable. To call the non-static methods.
 		UserRegistration registration = new UserRegistration();
-		// calling firstName method
+		// calling methods
 		registration.firstName();
+		registration.LastName();
 	}
 }
