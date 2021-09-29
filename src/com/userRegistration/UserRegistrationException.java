400bbe02c984ec3f6407268d21c515438cc51627
poojakondoju <kondojupoojitha612@gmail.com>
2021-09-29 11:50:06 +0530

    Added:Refactor the code to throw custom exceptions

---
+++ src/com/userRegistration/UserRegistrationException.java
@@ -0,0 +1,8 @@
+package com.userRegistration;
+
+public class UserRegistrationException extends Exception {
+	// Creating custom exception.
+	public UserRegistrationException(String message) {
+		super(message);
+	}
+}
